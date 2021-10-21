a27e43760b54311cbb2d898fc5d077313a25a7af
Stefan-Alexandru-Valentin-Curca <82599587+Stefan-Alexandru-Valentin-Curca@users.noreply.github.com>
2021-10-21 17:15:15 +0200

    Add files via upload

---
+++ practica1/array.php
@@ -0,0 +1,32 @@
+<?php
+
+<?php
+  $nombre = $_POST["Nombre"];
+echo "La nombre del host es..." . $nombre;
+
+array 
+
+$_contador
+
+
+
+}
+$nombre = array ("Stefan","Juan","Pepito");
+echo '<table border="1">';
+foreach ( $a as $r ) {
+        echo '<tr>';
+        foreach ( $r as $v ) {
+                echo '<td>'.$v.'</td>';
+        }
+        echo '</tr>';
+}
+echo '</table>';
+?>
+
+
+
+
+
+
+
+
